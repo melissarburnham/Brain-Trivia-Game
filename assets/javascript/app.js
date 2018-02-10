4009d83ada66a9ed19a6bb7c137e8c46a15dae83
Melissa Burnham <melissaburnham@Melissas-MacBook-Air.local>
2018-02-09 22:32:00 -0500

    rough pseudo code started

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+//variables
+var correctCounter;
+var wrongCounter;
+var unansweredCounter;
+
+
+
+
+//functions
+
+//start button to start game
+
+//timer for each question
+
+//grab question with corresponding answer choices
+
+//click answer
+    //if user is correct, show picture, add to correct score, go to next question
+    //if user is wrong, show picture and correct answer, add to wrong score, go to next question
+    //if time runs out, show picture and correct answer, add to unanswered counter, go to next question
+    //restart timer
+
+
+//end of questions, display game data
+    //have button to try again and reset the game
+
+
+
